e87ba1567ff7f5f8772b9a7914eb9895e1e18fe9
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2024-09-05 20:40:18 +0200

    fix

--- src/app/comments/comments.store.ts
+++ src/app/comments/comments.store.ts
@@ -1,30 +1,35 @@
-import { addEntities, entityConfig, removeAllEntities, withEntities } from "@ngrx/signals/entities";
-import { BlogComment, BlogPost, BlogStore } from "../blog-post/blog-post.store";
-import { patchState, signalStore, type, withHooks, withMethods } from "@ngrx/signals";
-import { Signal, inject } from "@angular/core";
+import {
+  addEntities,
+  entityConfig,
+  removeAllEntities,
+  setAllEntities,
+  setEntities,
+  withEntities
+} from "@ngrx/signals/entities";
+import {BlogComment, BlogPost, BlogStore} from "../blog-post/blog-post.store";
+import {patchState, signalStore, type, withHooks, withMethods} from "@ngrx/signals";
+import {Signal, inject} from "@angular/core";
+import {rxMethod} from "@ngrx/signals/rxjs-interop";
+import {pipe, tap} from "rxjs";
 
 const commentsConfig = entityConfig({
-    entity: type<BlogComment>(),
-    collection: 'comments',
-    selectId: (comment) => comment.id
-  });
+  entity: type<BlogComment>(),
+  collection: 'comments',
+  selectId: (comment) => comment.id
+});
 
-  export const CommentsStore = signalStore(
+export const CommentsStore = signalStore(
     withEntities(commentsConfig),
     withMethods((store) => ({
-        sync(comments: Signal<never[]>): void {
-            console.log('aer', comments());
-            patchState(store, removeAllEntities(commentsConfig));
-            // patchState(store, addEntities(comments,commentsConfig));
-        }  
+      sync: rxMethod<BlogComment[]>(pipe(tap(value => console.log('aer', value)), tap(comments => patchState(store, setAllEntities(comments, commentsConfig)))))
     })),
     withHooks((store) => {
-        const globalStore = inject(BlogStore);
-        return {
-          onInit() {
-            store.sync(globalStore.comments);
-          }
-        };
+      const globalStore = inject(BlogStore);
+      return {
+        onInit() {
+          store.sync(globalStore.comments);
+        }
+      };
     })
-);
-    
+  )
+;
